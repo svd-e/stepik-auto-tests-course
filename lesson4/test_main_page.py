e2c1216b7eff6b9b9908d86750cbdd79fb7c6460
Dmitriy.Sviridov <svdimitry@gmail.com>
2021-08-26 15:00:33 +0300

    lesson 4.1 step 6. preparation ver. 0

---
+++ lesson4/test_main_page.py
@@ -0,0 +1,11 @@
+import time
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    time.sleep(5)
+
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
+
+# pytest -v --tb=line --language=en test_main_page.py
