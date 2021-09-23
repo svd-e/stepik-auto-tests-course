08a792e8b12a0998ccb99621254e879faefa580b
Dmitriy.Sviridov <svdimitry@gmail.com>
2021-09-23 12:59:58 +0530

    lesson 4.2 step 9. Jumping between pages ver. 0

--- lesson4/test_main_page.py
+++ lesson4/test_main_page.py
@@ -8,6 +8,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    # login_page.should_be_login_page() # Для перехода между страницами. один из вариантов. Выбран другой
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -15,14 +18,14 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def test_guest_should_see_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+# def test_guest_should_see_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_page()
 
 
 
 
 # pytest -v --tb=line --language=en test_main_page.py
-# lesson 4.2 step 6. Checking an element on a page ver. 0
+# "lesson 4.2 step 8. LoginPage realization ver. 0"
