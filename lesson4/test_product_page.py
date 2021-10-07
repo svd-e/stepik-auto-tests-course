31abcb7e3f81c07fb110a6c5135f8ee6a5af292f
Dmitriy.Sviridov <svdimitry@gmail.com>
2021-10-07 18:30:19 +0530

    lesson 4.3 step 4. Many promotions. Parameterization ver. 0

--- lesson4/test_product_page.py
+++ lesson4/test_product_page.py
@@ -1,10 +1,12 @@
 from pages.product_page import ProductPage
 # from pages.main_page import MainPage
 import time
+import pytest
 
 def test_guest_can_add_product_to_basket(browser):
     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
@@ -12,5 +14,26 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_right_cost()
     # time.sleep(5)
 
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_be_right_name()
+    page.should_be_right_cost()
+
+
+
 # pytest -v -s --tb=line --language=en test_product_page.py
-# "lesson 4.3 step 2. Adding product to cart ver. 0"
+# "lesson 4.3 step 4. Many promotions. Parameterization ver. 0"
